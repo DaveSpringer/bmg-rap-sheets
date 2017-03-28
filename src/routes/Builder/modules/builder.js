977869507f060207335a5da5d68701c13838982a
Dave Springer <daspring23@gmail.com>
2017-03-28 17:38:43 -0600

    Populating all trait text
    Now we're making objects gargantuan!
    
    All of the trait text is now being populated on the characters. It
    was a really easy change, requiring writing a few map functions.

--- src/routes/Builder/modules/builder.js
+++ src/routes/Builder/modules/builder.js
@@ -42,7 +42,6 @@ export const actions = {
 const ACTION_HANDLERS = {
   [SELECT_CREW] : (state, action) => {
     let charFilter = (character) => {
-      console.log(`Char in crew ${action.crew.id} = ${character.crews}? ${character.crews.includes(action.crew.id)}`)
       return character.crews.includes(action.crew.id) || (character.crews.includes('*') && !character.hates.includes(action.crew.id))
     }
     return Object.assign({}, state, {
@@ -74,8 +73,6 @@ const ACTION_HANDLERS = {
     }
     let newRep = newCharacters.reduce((repSum, character) => repSum + character.reputation, 0)
     let newFunding = newCharacters.reduce((fundSum, character) => fundSum + character.funding, 0)
-    console.log(JSON.stringify(newCharacters))
-    console.log(JSON.stringify(newAvailChars))
     return Object.assign({}, state, {
       characters : newCharacters,
       availableCharacters : newAvailChars,
@@ -84,8 +81,7 @@ const ACTION_HANDLERS = {
     })
   },
   [LOAD_RESOURCES] : (state, action) => {
-    return Object.assign({}, state, {
-      allTraits : [{"name":"Bat-Armor MK 1","phase":"Execute the plan","cost":"","rule":"Each time this [Character] receives a [Hit], after [Block]s, [Ping]s, and [Dodge]s; roll a die. On a result of 5+, discard the [Hit].","grants":[]},
+    let allTraits = [{"name":"Bat-Armor MK 1","phase":"Execute the plan","cost":"","rule":"Each time this [Character] receives a [Hit], after [Block]s, [Ping]s, and [Dodge]s; roll a die. On a result of 5+, discard the [Hit].","grants":[]},
       {"name":"Bat-Armor MK III","phase":"Execute the plan","cost":"","rule":"Each time this [Character] receives a [Hit], after [Block]s, [Ping]s, and [Dodge]s; roll a die. On a result of 5+, discard the [Hit]. If this [Character] makes a Special Attack: Sneak Attack, they may assign Melee [Hit]s to targets up to 3cm away.  All of these [Hit]s are considered Sneak Attacks.","grants":[]},
       {"name":"Bat Cape","phase":"Execute the plan","cost":"1SC Passive","rule":"When this [Character] [Falls], they may spend 1 [SC] to avoid being [Knocked Down] and taking [Falling Damage].","grants":[]},
       {"name":"Batclaw","phase":"Execute the plan","cost":"1SC+1MC","rule":"Spend [1SC+1MC] to make a [Batclaw] movement. [See BMG pg 56]","grants":[]},
@@ -93,12 +89,27 @@ const ACTION_HANDLERS = {
       {"name":"Detective","phase":"Execute the plan","cost":"","rule":"For each [Detective] [Character] in your [Crew], you may cancel an [Opponent]'s [Pass] at the beginning of each [Round]. [Detective]s my reroll [Riddle Objectives] once.","grants":[]},
       {"name":"Martial Artist","phase":"Execute the plan","cost":"","rule":"This [Character] ignores [Outnumbered].","grants":[]},
       {"name":"Reinforced Gloves","phase":"Execute the plan","cost":"","rule":"This [Character]'s [Unarmed Attacks] produce 2 [Stun Damage].","grants":[]},
-      {"name":"Sustained Defenses","phase":"Execute the plan","cost":"","rule":"This [Character] receives 3 [Defense Dice] for every 2 [DC] spend [Defending] an [Attack].","grants":[]}],
-      allCrews : [{"name":"Batman","id":"bt"},{"name":"Green Arrow","id":"ga"},{"name":"Law Force","id":"lf"},{"name":"Joker","id":"jk"},{"name":"Penguin","id":"pn"},
+      {"name":"Sustained Defenses","phase":"Execute the plan","cost":"","rule":"This [Character] receives 3 [Defense Dice] for every 2 [DC] spend [Defending] an [Attack].","grants":[]}]
+      let allCrews = [{"name":"Batman","id":"bt"},{"name":"Green Arrow","id":"ga"},{"name":"Law Force","id":"lf"},{"name":"Joker","id":"jk"},{"name":"Penguin","id":"pn"},
       {"name":"League of Shadows","id":"ls"},{"name":"Bane","id":"bn"},{"name":"Poison Ivy","id":"pi"},{"name":"Court of Owls","id":"co"},{"name":"Mr. Freeze","id":"mf"},
       {"name":"Organized Crime","id":"oc"},{"name":"Two Face","id":"tf"},{"name":"Black Mask","id":"bm"},{"name":"Watchmen","id":"wm"},{"name":"Riddler","id":"rd"},
-      {"name":"Scarecrow","id":"sc"},{"name":"Wonderland Gang","id":"wg"},{"name":"LexCorp","id":"lc"},{"name":"Gorilla Grodd","id":"gg"},{"name":"Suicide Squad","id":"ss"}],
-      allCharacters : [{"name":"Bruce Wayne","alias":"Batman (Ben Affleck)","rank":"Leader","reputation":125,"funding":0,"crews":["bt"],"traits":["Bat-Armor MK 1","Bat Cape","Batclaw","Close Combat Master","Detective","Martial Artist","Reinforced Gloves","Sustained Defenses"]},{"name":"Selina Kyle","alias":"Catwoman","rank":"Free Agent","reputation":66,"funding":0,"crews":["*"],"hates":["lf","jk"],"traits":["Total Vision","Climbing Claws","Sneak Attack","Acrobat","Thief","Primary Target (Loot)","Retractable Claws"]}]
+      {"name":"Scarecrow","id":"sc"},{"name":"Wonderland Gang","id":"wg"},{"name":"LexCorp","id":"lc"},{"name":"Gorilla Grodd","id":"gg"},{"name":"Suicide Squad","id":"ss"}]
+      let loadedCharacters = [{"name":"Bruce Wayne","alias":"Batman (Ben Affleck)","rank":"Leader","reputation":125,"funding":0,"crews":["bt"],"traits":["Bat-Armor MK 1","Bat Cape","Batclaw","Close Combat Master","Detective","Martial Artist","Reinforced Gloves","Sustained Defenses"]},{"name":"Selina Kyle","alias":"Catwoman","rank":"Free Agent","reputation":66,"funding":0,"crews":["*"],"hates":["lf","jk"],"traits":["Total Vision","Climbing Claws","Sneak Attack","Acrobat","Thief","Primary Target (Loot)","Retractable Claws"]}]
+      //Now... Add all traits to each character... Ooph...
+      // TODO: Optimize the %@#$^ out of this in the future.
+      let findTrait = (traitName) => {
+        return allTraits.find((trait) => (traitName === trait.name))
+      }
+      let populateTraits = (character) => {
+        let poppedTraits = character.traits.map(findTrait)
+        character.traitText = poppedTraits
+        return character
+      }
+      let allCharacters = loadedCharacters.map(populateTraits)
+    return Object.assign({}, state, {
+      allTraits : allTraits,
+      allCrews : allCrews,
+      allCharacters : loadedCharacters
     })
   },
 }
