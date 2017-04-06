76652c3e1bfcad250f9f64a4310175eefbcb7198
Dave Springer <daspring23@gmail.com>
2017-04-05 19:25:38 -0700

    Expanding available characters
    Starting to add the Joker crew.
    
    Truthfully, this is an in-progress commit so that I can safely work
    on gutting a bunch of crap out of the system.
    
    Be very afraid...

--- src/resources/characters.js
+++ src/resources/characters.js
@@ -47,6 +47,119 @@ export const loadedCharacters = [{
       funding: 0,
       crews: [ 'bt' ],
       traits: [ 'Charismatic', { name : 'Combo with', weapon: 'Electric Baton' }, 'Explosive Gel', 'Acrobat', 'Batclaw', 'Sneak Attack' ]
-    }]
+    }, {
+      name: 'Damian Wayne',
+      alias: 'Robin',
+      rank: 'Free Agent',
+      reputation: 50,
+      funding: 0,
+      crews: [ 'bt' ],
+      traits: [ 'Martial Artist', 'Acrobat', 'Batclaw', { name: 'True Love', love: 'Talia Al Ghul' }, { name: 'True Love', love: 'Batman' }, 'Small', 'Detective', 'Master Fighter', 'Technique' ]
+    }, {
+      name: 'Alfred Pennyworth',
+      alias: 'None',
+      rank: 'Henchman',
+      reputation: 30,
+      funding: 0,
+      crews: [ 'bt' ],
+      traits: [ 'Informer', 'Veteran' ]
+    }, {
+      name: 'Jenette Vassili',
+      alias: 'Quick Response Team 3',
+      rank: 'Henchman',
+      reputation: 35,
+      funding: 400,
+      crews: [ 'lf', 'ga', 'bt' ],
+      traits: [ 'Bulletproof Vest', 'Radio', 'Tracking' ]
+    }, {
+      name: 'Salvador',
+      alias: 'Quick Response Team 2',
+      rank: 'Henchman',
+      reputation: 33,
+      funding: 350,
+      crews: [ 'lf', 'ga', 'bt' ],
+      traits: [ 'Bulletproof Vest', 'Street Guy' ]
+    }, {
+      name: 'Agent O Connell',
+      alias: 'Unknown',
+      rank: 'Henchman',
+      reputation: 29,
+      funding: 0,
+      crews: [ 'lf', 'ga', 'bt' ],
+      traits: [ 'Arrest', 'Veteran' ]
+    }, {
+      name: 'L. Lawrence',
+      alias: 'Quick Response Team 1',
+      rank: 'Henchman',
+      reputation: 27,
+      funding: 100,
+      crews: [ 'lf', 'ga', 'bt' ],
+      traits: [ 'Arrest', 'Bulletproof Vest' ]
+    }, {
+      name: 'Unknown',
+      alias: 'Gotham Policewoman',
+      rank: 'Henchman',
+      reputation: 14,
+      funding: 0,
+      crews: [ 'lf', 'ga', 'bt' ],
+      traits: [ 'Arrest' ]
+    }, {
+      name: 'Unknown',
+      alias: 'Joker (Jared Leto)',
+      rank: 'Leader',
+      reputation: 120,
+      funding: 0,
+      crews: [ 'jk' ],
+      traits: [ 'Mobster', { name: 'Elite Boss', type: 'Cosplayers'}, 'Kaos Agent', 'Follow Me!', 'Psycho', 'Survivor', { name: 'True Love', love: 'Harley Quinn'} ]
+    }, {
+      name: 'Dr. Harleen Frances Quinzel',
+      alias: 'Harley Quinn',
+      rank: 'Sidekick',
+      reputation: 90,
+      funding: 300,
+      crews: [ 'jk', 'ss' ],
+      traits: [ 'Acrobat', 'Confusion', 'Charismatic', 'Combat Flip', 'Goad', 'Suicide Squad member', { name: 'True Love', love: 'Joker' } ]
+    }, {
+      name: 'Unknown',
+      alias: 'Panda Man',
+      rank: 'Henchman',
+      reputation: 40,
+      funding: 650,
+      crews: [ 'jk' ],
+      traits: [ { name: 'Elite', type: 'Cosplayers'}, 'Hardened', 'Paranoid (Mental Disorder)' ]
+    }, {
+      name: 'Unknown',
+      alias: 'Goat Head Priest',
+      rank: 'Henchman',
+      reputation: 35,
+      funding: 400,
+      crews: [ 'jk' ],
+      traits: [ { name: 'Elite', type: 'Cosplayers'}, 'Dirty Fighter' ]
+    }, {
+      name: 'Stan',
+      alias: 'The Eyeball Man',
+      rank: 'Henchman',
+      reputation: 27,
+      funding: 350,
+      crews: [ 'jk' ],
+      traits: [ { name: 'Elite', type: 'Cosplayers'}, 'Street Guy' ]
+    }, {
+      name: 'Unknown',
+      alias: 'Mr. Grin (white)',
+      rank: 'Henchman',
+      reputation: 19,
+      funding: 0,
+      crews: [ 'jk' ],
+      traits: [ { name: 'Elite', type: 'Cosplayers'}, 'Desensitized' ]
+    }, {
+      name: 'Unknown',
+      alias: 'Mr. Grin (black)',
+      rank: 'Henchman',
+      reputation: 17,
+      funding: 0,
+      crews: [ 'jk' ],
+      traits: [ { name: 'Elite', type: 'Cosplayers'}, 'Desensitized' ]
+    }
+  ]
 
 // console.log(JSON.stringify(loadedCharacters))
--- src/resources/traits.js
+++ src/resources/traits.js
@@ -13,7 +13,7 @@ export const allTraits = [
     cost: "",
     rule: "Each time this [Character] receives a [Hit], after [Block]s, [Ping]s, and [Dodge]s; roll a die. On a result of 5+, discard the [Hit]. If this [Character] makes a Special Attack: Sneak Attack, they may assign Melee [Hit]s to targets up to 3cm away.  All of these [Hit]s are considered Sneak Attacks.",
     grants: [],
-    page: "?"
+    page: "G.26"
   },
   {
     name: "Bat Cape",
@@ -160,7 +160,7 @@ export const allTraits = [
     cost: "",
     rule: "During -target- [Character]'s turn, they may spend -counter- [Action Counters] of this [Character]'s as long as they are within 10cm.",
     grants: [],
-    page: "?"
+    page: "G.44"
   }, {
     name: "Elite Boss",
     nameFunc: "Elite Boss: (-type-)",
@@ -168,56 +168,56 @@ export const allTraits = [
     cost: "",
     rule: 'Your crew may contain any number of [Elite]: -type-',
     grants: [],
-    page: '?'
+    page: 'B.59'
   }, {
     name : 'Air Support',
     phase: 'Execute the plan',
     cost: '3SC',
     rule: 'Place a 10cm [Explosion Template] in the [Play Area]. [Target]s within the [Template] are [Illuminated].',
     grants: [],
-    page: '?'
+    page: 'B.55'
   }, {
     name : 'Arrest',
     phase: 'Execute the plan',
     cost: '1SC+1MC',
     rule: 'Remove a [K.O.] [Character] in [Contact] as a [Casualty].',
     grants: [],
-    page: '?'
+    page: 'B.55'
   }, {
     name : 'Kevlar Vest',
     phase: 'Execute the plan',
     cost: '',
     rule: 'Reduce [Damage Markers] recieved by 1 to a minimum of 1.',
     grants: [],
-    page: '?'
+    page: 'B.60'
   }, {
     name : 'Take Cover!!',
     phase: 'Execute the plan',
     cost: '1SC',
     rule: 'Once per [Turn], [Henchmen] within 10cm of this [Character] assign 1 [DC].',
     grants: [],
-    page: '?'
+    page: 'B.65'
   }, {
     name : 'Veteran',
     phase: 'Execute the plan',
     cost: '',
     rule: 'When this [Character] is [Activated], they may reassign 2 [Action Counters].',
     grants: [],
-    page: '?'
+    page: 'B.66'
   }, {
     name : 'Informer',
     phase: 'Raise the plan',
     cost: '',
     rule: 'As long as this [Character] is [Active], you gain 1 additional [Pass].',
     grants: [],
-    page: '?'
+    page: 'B.60'
   }, {
     name : 'Sealth',
     phase: 'Execute the plan',
     cost: '',
     rule: 'As long as this [Character] is [Active], you gain 1 additional [Pass].',
     grants: [],
-    page: '?'
+    page: 'B.65'
   }, {
     name : 'Combo with',
     nameFunc : 'Combi with: -weapon-',
@@ -225,7 +225,65 @@ export const allTraits = [
     cost: '',
     rule: 'When attacking with -weapon-, gain 1 extra [Attack Dice] for every 2 [AC] spent.',
     grants: [],
-    page: '?'
+    page: 'B.58'
+  }, {
+    name : 'True Love',
+    nameFunc: 'True Love: -love-',
+    phase: 'Execute the plan',
+    cost: '',
+    rule: 'If the [Character] and -love- are in the same [Crew], and -love- is removed as a casualty, this [Character] gains +1 [Willpower] and +1 [Attack] ([Stacking]) until the end of the game.',
+    grants: [],
+    page: 'B.66'
+  }, {
+    name : 'Small',
+    phase: 'Execute the plan',
+    cost: '',
+    rule: 'This [Character]\'s [BMD] is 8cm. This [Character] gets +1 to [Ping!] rolls',
+    grants: [],
+    page: 'B.65'
+  }, {
+    name : 'Technique',
+    phase: 'Execute the plan',
+    cost: '1SC',
+    rule: 'If this [Character] [Damages] an enemy with this [Close Combat] [Special Attack], the [Target] is [Knocked Down]. If the [Target] would be knocked down from [Collateral Damage], they suffer a hit at [Strength 4+] and suffer 1 [Stun].',
+    grants: [],
+    page: 'B.65'
+  }, {
+    name : 'Bulletproof Vest',
+    phase: 'Execute the plan',
+    cost: '',
+    rule: 'Firearms require a 4+ to [Damage] this [Character].',
+    grants: [],
+    page: 'B.58'
+  }, {
+    name : 'Radio',
+    phase: 'Execute the plan',
+    cost: '',
+    rule: 'This model can reroll [Lets Go!] rolls.',
+    grants: [],
+    page: 'G.42'
+  }, {
+    name : 'Tracking',
+    phase: 'VP and Casualty',
+    cost: '2SC',
+    rule: 'At the end of the [VP and Casualty] phase, this [Character] can move up to 2D6 cm.',
+    grants: [],
+    page: 'G.44'
+  }, {
+    name : 'Street Guy',
+    phase: 'Execute the plan',
+    cost: '',
+    rule: 'This [Character] can [Crouch] without spending [SC].',
+    grants: [],
+    page: 'B.65'
+  }, {
+    name : 'Elite',
+    nameFunc: 'Elite: -type-',
+    phase: 'Crew Building',
+    cost: '',
+    rule: 'Your [Crew] may only include 1 [Elite] of each type.',
+    grants: [],
+    page: 'B.59',
   }
 ]
 
