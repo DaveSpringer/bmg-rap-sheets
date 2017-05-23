dbbf50a755e5d458daa58449265f4074d17558fa
Dave Springer <daspring23@gmail.com>
2017-05-23 10:26:49 -0700

    Another Heroku attempt
    Keep trying, you'll get better.
    
      -- Daniel Tiger, 2013

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -2,6 +2,7 @@ const logger = require('../lib/logger')
 
 logger.info('Starting server...')
 var port = process.env.PORT || 5000
-require('../../server/main').listen(port, () => {
+var host = '0.0.0.0'
+require('../../server/main').listen(port, host, () => {
   logger.success('Server is running at http://localhost:' + port)
 })
